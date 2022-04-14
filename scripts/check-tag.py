0ead5cab1cfaaefc07226602ec1ba1b235158c70
Simon Cozens <simon@simon-cozens.org>
2022-04-14 16:14:26 +0100

    Identify config file and pass to GitHub

--- scripts/check-tag.py
+++ scripts/check-tag.py
@@ -3,6 +3,8 @@ import sys
 from fontv.libfv import FontVersion
 from fontTools.ttLib import TTFont
 from glob import glob
+import yaml
+
 
 tag = sys.argv[1]
 try:
@@ -25,6 +27,17 @@ if not a_font:
 ttf_version = FontVersion(a_font[0]).version
 real_family_name = TTFont(a_font[0])["name"].getDebugName(1)
 
+# Identify the config file which created this
+found_config = None
+for config in glob("sources/*yaml"):
+	this_config = yaml.load(open(config), Loader=yaml.FullLoader)
+	if "familyName" in this_config and this_config["familyName"] == real_family_name:
+		found_config = config
+		break
+if not found_config:
+	print(f"::error file=scripts/checktag.py::Could not identify config file for {family}")
+	sys.exit(1)
+
 if "Version "+version != ttf_version:
 	print(f"::error file=scripts/checktag.py::Trying to create release for version {version}, found font version {ttf_version}")
 	sys.exit(1)
@@ -32,4 +45,5 @@ if "Version "+version != ttf_version:
 print("::set-output name=result::pass")
 print(f"::set-output name=family::{family}")
 print(f"::set-output name=real_family_name::{real_family_name}")
+print(f"::set-output name=config_file::{found_config}")
 sys.exit(0)
